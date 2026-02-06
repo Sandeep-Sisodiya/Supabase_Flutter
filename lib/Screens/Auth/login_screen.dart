a1f23efc056e7f4f79437b693301487e7363a1a8
Sandeep Sisodiya <sandeepsisodiya16092004@gmail.com>
2026-02-07 00:14:41 +0530

    commit

--- lib/Screens/Auth/login_screen.dart
+++ lib/Screens/Auth/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import '../Home/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,36 +13,61 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final email = TextEditingController();
   final password = TextEditingController();
+  bool loading = false;
+  final supabase = Supabase.instance.client;
 
-
-
-
+  login() async {
+    setState(() {
+      loading = true;
+    });
+    try {
+      final result = await supabase.auth.signInWithPassword(
+        email: email.text,
+        password: password.text,
+      );
+      if (result.user != null && result.session != null) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+          (context) => false,
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+    } finally {
+      setState(() {
+        loading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-
-      ),
+      appBar: AppBar(title: Text('Login')),
       body: ListView(
         padding: EdgeInsets.all(15),
         children: [
           TextFormField(
             controller: email,
-            decoration: InputDecoration(
-              hintText: 'Email'
-            ),
+            decoration: InputDecoration(hintText: 'Email'),
           ),
-          SizedBox(height: 15,),
+          SizedBox(height: 15),
           TextFormField(
             controller: password,
-            decoration: InputDecoration(
-                hintText: 'Password'
-            ),
+            decoration: InputDecoration(hintText: 'Password'),
           ),
-          SizedBox(height: 20,),
-          ElevatedButton(onPressed: (){}, child:  Text('Login'))
+          SizedBox(height: 20),
+          loading
+              ? CircularProgressIndicator():
+          ElevatedButton(onPressed: () {
+            login();
+          }, child: Text('Login')),
+          SizedBox(height: 20),
+          TextButton(
+            onPressed: () {},
+            child: Text('Don\'t have an account? Register'),
+          ),
         ],
       ),
     );
